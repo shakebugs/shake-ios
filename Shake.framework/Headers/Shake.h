5153b068f80ba4bb6be4b223921535bcda42824c
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2019-05-09 04:25:41 -0700

    5.0.0

---
+++ Shake.framework/Headers/Shake.h
@@ -0,0 +1,41 @@
+//
+//  Shake.h
+//  ShakeFramework
+//
+//  Created by David Halapir on 19/11/2018.
+//  Copyright © 2018 DECODE HQ Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ShakeReportData.h"
+#import "ShakeEnums.h"
+
+//! Project version number for Shake.
+FOUNDATION_EXPORT double ShakeVersionNumber;
+
+//! Project version string for Shake.
+FOUNDATION_EXPORT const unsigned char ShakeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shake/PublicHeader.h>
+
+NS_ASSUME_NONNULL_BEGIN
+NS_SWIFT_NAME(Shake)
+@interface SHKShake : NSObject
+
+/**
+ Add additional data and files to the report object provided by the block.
+ */
+@property (nonatomic, copy) SHKShakeReportData *(^onPrepareData)(SHKShakeReportData *reportData);
+
++ (void)start;
++ (void)startWithInvocationEvents:(ShakeInvocationEvent)invocationEvents;
++ (void)manualStart;
++ (SHKShake *)sharedInstance;
++ (void)setBlackBoxEnabled:(BOOL)isBlackBoxEnabled;
++ (void)autoReportWithAttachScreenshot:(BOOL)screenshot description:(NSString *)description;
++ (void)autoReportWithAttachScreenshot:(BOOL)screenshot;
++ (void)autoReportWithDescription:(NSString *)description;
++ (void)autoReport;
+@end
+
+NS_ASSUME_NONNULL_END
